1bef91f2b821ee3c6650964a94f1d28d97415a9a
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2023-03-17 10:59:15 +0100

    Update SshConnectionCommand.php

--- symfony_project/src/Command/SshConnectionCommand.php
+++ symfony_project/src/Command/SshConnectionCommand.php
@@ -7,7 +7,11 @@ class SshConnectionCommand
     private $username;
     private $password;
     private $ssh;
-
+    private $database_name = 'my_new_database';
+    private $create_database_command = 'mysql -u root -p -e "CREATE DATABASE ' . $database_name . ';"';
+    private $result = $ssh->exec($create_database_command);
+    
+  
     public function __construct($host, $username, $password)
     {
         $this->host = $host;
@@ -21,6 +25,9 @@ class SshConnectionCommand
         if (!$this->ssh->login($this->username, $this->password)) {
             throw new \RuntimeException('Unable to connect to SSH server');
         }
+        else{
+                echo 'SSH connection established successfully.';
+        }
     }
 
     public function exec($command)
@@ -31,5 +38,13 @@ class SshConnectionCommand
 
         $output = $this->ssh->exec($command);
         return $output;
+    }
+
+    public function dbConnexion($result){
+        if ($result) {
+            echo 'Database created successfully.';
+        } else {
+            echo 'Error creating database: ' . $ssh->getLastError();
+        }
     }
 }
